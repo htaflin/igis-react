8a3e2e9a42d830040820c14c5fd1c4ea1f7023bc
parkerziegler <parkerellisziegler@gmail.com>
2017-10-24 16:49:11 -0700

    Intro to props and state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Header from './components/Header.jsx';
 
 class App extends Component {
   render() {
+
+    let items = [{value: 'Parker', color: '#A1A1A1', message: 'is cool'}, {value: 'Jason', color: '#Fc7f2D', message: 'is clever'}, {value: 'Erich', color: '#000000', message: 'is a secret genius.'}, {value: 'Helena', color: "#456123", message: 'has cream cheese on her face'}];
+
+    let jsxElements = items.map((item, i) => {
+      
+      return <Header value={item.value} color={item.color} message={item.message} key={i} />;
+    });
+
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World!</h1>
-    </div>
+      <div style={{textAlign: 'center'}}>
+        {jsxElements}
+      </div>
     );
   }
 }
