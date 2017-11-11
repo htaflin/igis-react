acd03dde6b6238f35e243eb8bdd61b2f38231002
Jason Berney <jberney@integralgis.local>
2017-11-10 16:31:34 -0800

    Adding Item and ItemList components.

---
+++ src/components/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Item extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            props.item
+        }
+    }
+}
+
+export default Item;
