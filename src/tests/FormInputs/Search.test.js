50abd7e5e21fe0b12045dd180c14a006c2029217
parkerziegler <parkerellisziegler@gmail.com>
2017-11-16 21:07:08 -0800

    Checking in test for controlled text input.

--- src/tests/FormInputs/Search.test.js
+++ src/tests/FormInputs/Search.test.js
@@ -15,7 +15,18 @@ test('Clicking cancel button clears search input', () => {
 
     const search = shallow(<Search />);
 
+    console.log(search.debug());
+
     search.find('button').simulate('click');
 
     expect(search.state().typed).toEqual('');
+});
+
+test('Typing should update state', () => {
+
+    const search = shallow(<Search />);
+
+    search.find('input').simulate('change', { target: { value: 'hi Chris!' }});
+    
+    expect(search.state().typed).toEqual('hi Chris!');
 });
